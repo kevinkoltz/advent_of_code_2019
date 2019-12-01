cbf21606bd1dcf42b77853a0d8093f14edb77c6b
Kevin Koltz <kevin.koltz@gmail.com>
2019-12-01 09:35:36 -0600

    Initial mix project

---
+++ lib/aoc19.ex
@@ -0,0 +1,18 @@
+defmodule Aoc19 do
+  @moduledoc """
+  Documentation for Aoc19.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Aoc19.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/aoc19_test.exs
@@ -0,0 +1,8 @@
+defmodule Aoc19Test do
+  use ExUnit.Case
+  doctest Aoc19
+
+  test "greets the world" do
+    assert Aoc19.hello() == :world
+  end
+end
