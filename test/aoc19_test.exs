ac4d31d2f6d8bcae6abe2b0af270cda13f8c62f2
Kevin Koltz <kevin.koltz@gmail.com>
2019-12-03 00:45:41 -0600

    Solve day 2 part 1

--- test/aoc19_test.exs
+++ test/aoc19_test.exs
@@ -2,14 +2,10 @@ defmodule Aoc19Test do
   use ExUnit.Case
   doctest Aoc19
   doctest RocketModule
+  doctest Intcode
 
   test "day 1" do
-    masses =
-      "day_01_input.txt"
-      |> File.read!()
-      |> String.trim()
-      |> String.split("\n")
-      |> Enum.map(&Decimal.new(&1))
+    masses = read_puzzle_input("day_01_input.txt", delim: "\n", formatter: &Decimal.new/1)
 
     rocket = Rocket.new(masses)
 
@@ -18,5 +14,27 @@ defmodule Aoc19Test do
 
     # part 2
     assert Rocket.total_fuel_required(rocket) == Decimal.new(4_953_118)
+  end
+
+  test "day 2" do
+    program = read_puzzle_input("day_02_input.txt", delim: ",", formatter: &String.to_integer/1)
+
+    # part 1
+    assert [position_0 | _] = Intcode.restore(program, 12, 2)
+    assert position_0 == 5_434_663
+  end
+
+  @doc """
+  Reads puzzle input from a text file.
+  """
+  def read_puzzle_input(filename, opts) do
+    delim = Keyword.fetch!(opts, :delim)
+    formatter = Keyword.fetch!(opts, :formatter)
+
+    filename
+    |> File.read!()
+    |> String.trim()
+    |> String.split(delim)
+    |> Enum.map(&formatter.(&1))
   end
 end
